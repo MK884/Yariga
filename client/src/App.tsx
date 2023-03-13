7c99c5f6f06913f021a9fbdcb4d94930de261c62
MK884 <marchantkhalid7810@gmail.com>
2023-03-13 23:30:16 +0530

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -58,7 +58,7 @@ function App() {
 
       // save user to MongoDB....
       if(profileObj){
-        const response = await fetch('http://localhost:8080/api/v1/users',{
+        const response = await fetch('https://yariga-weri.onrender.com/api/v1/users',{
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -132,7 +132,7 @@ function App() {
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
           <Refine
-            dataProvider={dataProvider("http://localhost:8080/api/v1")}
+            dataProvider={dataProvider("https://yariga-weri.onrender.com/api/v1")}
             notificationProvider={notificationProvider}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
